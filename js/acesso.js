09850f9a17dd468e7a6636b6925dda41e18820ab
Gabriel Costa de Souza <86692993+GbrlSouza@users.noreply.github.com>
2023-03-29 23:02:21 -0300

    Add files via upload

--- js/acesso.js
+++ js/acesso.js
@@ -6,7 +6,7 @@ function login() {
         alert("Por favor, preencha os campos de usuário e senha.");
     } else if (username === "infomaq" && password === "06069") {
         alert("Login bem sucedido.");
-        window.open("./../source/pages/restrito/index.html");
+        window.open("./../restrito/index.html");
     } else {
         alert("Usuário ou senha incorretos. Tente novamente.");
     }
